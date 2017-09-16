a425198eb148f2fe4dc01ac1739cae8cb4f71403
Dmitry Gornostaipolsky <d.gornostaipolskiy@gmail.com>
2017-09-16 22:03:57 +0300

    Add modules. Add stage and prod configurations

---
+++ terraform/modules/app/main.tf
@@ -0,0 +1,34 @@
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "europe-west1-b"
+  tags         = ["reddit-app"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.app_disk_image}"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config = {}
+  }
+
+  metadata {
+    sshKeys = "appuser:${file(var.public_key_path)}"
+  }
+}
+
+resource "google_compute_firewall" "firewall_puma" {
+  name    = "allow-puma-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["reddit-app"]
+}
